335d39c273883ed2da1d5d225d7321ab67c4f5f9
rajesh <rchaganti@evoketechnologies.com>
2026-01-03 18:55:57 -0500

    added this to fix error in workorder app console

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -1,9 +1,11 @@
 export const mfConfig = {
   name: "home_app",
+  filename: "remoteEntry.js",
   exposes: {
     "./Header": "./src/components/Header.tsx",
     "./Footer": "./src/components/Footer.tsx",
     
   },
   shared: ["react", "react-dom"],
+  manifest: true,
 };
